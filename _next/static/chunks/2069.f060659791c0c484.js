99077f95085058435eb0d5de5bf3334d31c85f50
tomas <tomas@graphite.dev>
2025-05-04 20:59:15 -0400

    feat: upgrade to nextra4

---
+++ _next/static/chunks/2069.f060659791c0c484.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[2069],{82069:function(e,c,r){r.d(c,{createArchitectureServices:function(){return t.i}});var t=r(79010);r(95592)}}]);
