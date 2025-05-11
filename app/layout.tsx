8213a4d8e2e7b1089602367aeadfbc49a82189b1
tomas <tomas@graphite.dev>
2025-05-11 13:54:02 -0400

    fix: favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -33,7 +33,13 @@ export default async function RootLayout({ children }) {
   const pageMap = await getPageMap();
   return (
     <html lang="en" dir="ltr" suppressHydrationWarning>
-      <Head faviconGlyph="📚" />
+      <Head faviconGlyph="📚">
+        {/* The default Nextra 4 favicon does not work with Chrome (it uses .svg in the embedded css rather than .text) */}
+        <link
+          rel="icon"
+          href={`data:image/svg+xml;utf8,<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 100 100'><text x='50' y='.9em' font-size='90' text-anchor='middle'>📚</text><style>text{font-family:system-ui,-apple-system,BlinkMacSystemFont,"Segoe UI",Roboto,"Helvetica Neue",Arial,"Noto Sans",sans-serif,"Apple Color Emoji","Segoe UI Emoji","Segoe UI Symbol","Noto Color Emoji";fill:black}@media(prefers-color-scheme:dark){text{fill:white}}</style></svg>`}
+        />
+      </Head>
       <body className={inter.className}>
         <Layout
           navbar={navbar}
